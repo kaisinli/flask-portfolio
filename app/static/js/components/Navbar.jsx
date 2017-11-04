57d61055d64cb9e971e6d08f11195012b8d71074
--global <kaixinli91@gmail.com>
2017-11-04 16:14:19 -0400

    adds

--- app/static/js/components/Navbar.jsx
+++ app/static/js/components/Navbar.jsx
@@ -50,7 +50,7 @@ class Navbar extends Component {
                                 </a>
                             </li>
                             <li>
-                                <a id="nav-resume" href="https://goo.gl/P3RgDt" target="_blank">Resume</a>
+                                <a id="nav-resume" href="https://drive.google.com/open?id=0B1l3YuhgCB5PYlhETm14UnJad0E" target="_blank">Resume</a>
                             </li>
                         </ul>
                     </div>
